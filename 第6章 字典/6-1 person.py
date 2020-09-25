761269c6a5bc1b52232e31d26983400eb4d6b0ec
Jeffery <1025958194@qq.com>
2020-09-25 23:56:52 +0800

    day7

---
+++ 第6章 字典/6-1 person.py
@@ -0,0 +1,6 @@
+person = {'first_name': 'Li', 'last_name': 'Junwei',
+          'age': 28, 'city': 'Shanghai'}
+print("fisrt name: " + person['first_name'])
+print("last name: " + person['last_name'])
+print("age: " + str(person['age']))
+print("city: " + person['city'])
---
+++ 第6章 字典/6-2 favority_number.py
@@ -0,0 +1,12 @@
+favorite_number = {'Susan': 23, 'Lily': 45, 'Tom': 5,
+                   'Edward': 8, 'Jerry': 4}
+print("Susan's favorite number is " +
+      str(favorite_number['Susan']))
+print("Lily's favorite number is " +
+      str(favorite_number['Lily']))
+print("Tom's favorite number is " +
+      str(favorite_number['Tom']))
+print("Edward's favorite number is " +
+      str(favorite_number['Edward']))
+print("Jerry's favorite number is " +
+      str(favorite_number['Jerry']))
